c722731e6ca262b4782bb535a3bfb62e8e65d4ee
Piotr <piotrpopis98@icloud.com>
2020-06-25 21:15:53 +0200

    random model (initial solution (simple))

--- nnb.py
+++ nnb.py
@@ -1,12 +1,20 @@
 import argparse
+import random
 
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import pyplot
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.utils import plot_model
 from tensorflow.python.keras import Sequential
 from tensorflow.python.keras.layers import Dense
+from vis.utils.utils import apply_modifications
+
+
+def update_layer_activation(model, activation, index=-1):
+    model.layers[index].activation = activation
+    return apply_modifications(model)
 
 
 def plot_(x, y, scx, scy, model):
@@ -21,16 +29,6 @@ def plot_(x, y, scx, scy, model):
     plt.ylabel('Output Variable (y)')
     plt.legend()
     plt.show()
-
-
-def get_random_model(x, y, ep=500):
-    model = Sequential()
-    model.add(Dense(10, input_dim=1, activation='relu', kernel_initializer='he_uniform'))
-    model.add(Dense(10, activation='relu', kernel_initializer='he_uniform'))
-    model.add(Dense(1))
-    model.compile(loss='mse', optimizer='adam')
-    model.fit(x, y, epochs=ep, batch_size=10, verbose=0)
-    return model
 
 
 def get_data(parsed_args):
@@ -58,12 +56,41 @@ def reshape(x, y):
 
 
 def fit_trans(x, y, scale_x, scale_y):
-    # learn to use mean sometimes we have nan so we changing it to means.
     return scale_x.fit_transform(x), scale_y.fit_transform(y)
 
 
 def inv_trans(x, y, scale_x, scale_y):
     return scale_x.inverse_transform(x), scale_y.inverse_transform(y)
+
+
+def get_activation_random():
+    activations_str_list = ['sigmoid', 'relu', 'tanh']  # , 'elu', 'selu', 'softmax', 'softplus']
+    return random.choice(activations_str_list)
+
+
+def get_random_neurons(bottom_limit=2, upper_limit=128):
+    return random.randint(bottom_limit, upper_limit)
+
+
+def create_model(neurons_quantity_list, activations_list, krnl='he_uniform', in_dim=1, out_dim=1):
+    if len(neurons_quantity_list) != len(activations_list):
+        raise IndexError('')
+    model = Sequential()
+    model.add(Dense(neurons_quantity_list[0], input_dim=in_dim, activation=activations_list[0],
+                    kernel_initializer=krnl))
+    for n, ac in zip(neurons_quantity_list[1:], activations_list[1:]):
+        model.add(Dense(n, activation=ac, kernel_initializer=krnl))
+    model.add(Dense(out_dim))
+    return model
+
+
+def get_random_model(up_lim=12, bottom_lim=1):
+    layers_no = random.randint(bottom_lim, up_lim)
+    acts, neurs = [], []
+    for _ in range(layers_no):
+        acts.append(get_activation_random())
+        neurs.append(get_random_neurons())
+    return create_model(neurs, acts)
 
 
 def main():
@@ -73,15 +100,12 @@ def main():
     arp.add_argument('-plot', action='store_true')
     p = arp.parse_args()
     x, y = get_data(p)
+    print(x, y)
     x, y = reshape(x, y)
     scale_x, scale_y = scale_data(x, y, p.expression)
     x, y = fit_trans(x, y, scale_x, scale_y)
     print(x.min(), x.max(), y.min(), y.max())
-    # design the neural network model
-    model = Sequential()
-    model.add(Dense(10, input_dim=1, activation='relu', kernel_initializer='he_uniform'))
-    model.add(Dense(10, activation='relu', kernel_initializer='he_uniform'))
-    model.add(Dense(1))
+    model = get_random_model()
     # define the loss function and optimization algorithm
     model.compile(loss='mse', optimizer='adam')
     # ft the model on the training dataset
@@ -92,6 +116,11 @@ def main():
     x_plot, y_plot = inv_trans(x, y, scale_x, scale_y)
     yhat_plot = scale_y.inverse_transform(yhat)
     # report model error
+    plot_model(
+        model, to_file='model.png', show_shapes=True, show_layer_names=True,
+        rankdir='LR', expand_nested=True, dpi=96
+    )
+
     print('MSE: %.3f' % mean_squared_error(y_plot, yhat_plot))
     # plot x vs y
     pyplot.scatter(x_plot, y_plot, label='Actual')

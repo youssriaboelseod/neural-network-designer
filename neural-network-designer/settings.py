a04fe4cc23c393f6080581343ea2ccbf24112fbb
Piotr <piotrpopis98@icloud.com>
2020-07-12 08:53:22 +0200

    reg + login not finished

--- neural-network-designer/settings.py
+++ neural-network-designer/settings.py
@@ -67,14 +67,21 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'neural-network-designer.wsgi.application'
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nnb_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
